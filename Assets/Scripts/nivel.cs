119b4d64ff23ae5827d279fbff074e970cd4451e
AJHunter <antoniojbfs04@gmail.com>
2025-11-04 09:36:56 -0300

    passar de nivel
    resumindo, eu fiz um objeto que quando colidido você pode ir para outra cena e você pode escolher qual faze será

---
+++ Assets/Scripts/nivel.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class nivel : MonoBehaviour
+{
+    public bool fase1 = false;
+    public bool fase2 = false;
+    public bool fase3 = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // me pediram para fazer um negocio que quando o player colide passar de fase
+        // mas pelo que eu entedi vai ter mais de uma fase então eu fiz isso
+        // voce pode escolher qual fase sera quando colidir
+
+        if (fase1 == true)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene("Game");//coloca o nome da cena ai
+            }
+        }
+        if (fase2 == true)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene("");//coloca o nome da cena ai
+            }
+        }
+        if (fase3 == true)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                SceneManager.LoadScene("");//coloca o nome da cena ai
+            }
+        }
+
+
+
+    }
+}
