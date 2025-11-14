869f191965aae27eb5805dd6b3b61f654a8d8eb4
Madara Lima <maisumemail333@gmail.com>
2025-11-14 11:35:05 -0300

    NewGarage

--- Assets/LerpLight.cs
+++ Assets/LerpLight.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class LightFlicker : MonoBehaviour
 {
-    [Header("Configurações")]
-    public Light luz;                  // Referência à luz
-    public float minIntensidade = 0.5f; // Intensidade mínima
-    public float maxIntensidade = 2f;   // Intensidade máxima
+    [Header("Configurations")]
+    public Light luz;                  // Referência a luz
+    public float minIntensidade = 0.5f; // Intensidade minima
+    public float maxIntensidade = 2f;   // Intensidade maxima
     public float velocidade = 3f;       // Velocidade da variação
 
     private float alvo;                 // Intensidade alvo atual
@@ -20,10 +20,10 @@ public class LightFlicker : MonoBehaviour
 
     void Update()
     {
-        // Faz o Lerp da intensidade atual até o alvo
+        // Faz o Lerp da intensidade atual ate o alvo
         luz.intensity = Mathf.Lerp(luz.intensity, alvo, Time.deltaTime * velocidade);
 
-        // Quando a intensidade estiver próxima do alvo, escolhe um novo valor
+        // Quando a intensidade estiver proxima do alvo, escolhe um novo valor
         if (Mathf.Abs(luz.intensity - alvo) < 0.05f)
         {
             alvo = Random.Range(minIntensidade, maxIntensidade);
